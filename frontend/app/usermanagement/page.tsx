8af090244ee1b9eb0b288d6dd2b5e0e6e8581339
yukino <developer.yukine@gmail.com>
2024-08-27 13:18:40 +1000

    feat(demo): added demo page for user management

---
+++ frontend/app/usermanagement/page.tsx
@@ -0,0 +1,125 @@
+'use client';
+
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Autocomplete from '@mui/joy/Autocomplete';
+import Box from '@mui/joy/Box';
+import Chip from '@mui/joy/Chip';
+import ChipDelete from '@mui/joy/ChipDelete';
+import Typography from '@mui/joy/Typography';
+import Button from '@mui/joy/Button';
+import Stack from '@mui/joy/Stack';
+import RadioGroup from '@mui/joy/RadioGroup';
+import Radio from '@mui/joy/Radio';
+import Slider from '@mui/joy/Slider';
+import AccordionGroup from '@mui/joy/AccordionGroup';
+import Accordion from '@mui/joy/Accordion';
+import AccordionDetails, {
+    accordionDetailsClasses,
+} from '@mui/joy/AccordionDetails';
+import AccordionSummary, {
+    accordionSummaryClasses,
+} from '@mui/joy/AccordionSummary';
+
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import FolderRoundedIcon from '@mui/icons-material/FolderRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+
+import Layout from '@/app/components/layout';
+import Header from '@/app/components/header';
+import Navigation from '@/app/components/navigation';
+
+import UserList from './UserList';
+
+export default function UserManagementPage() {
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+    const initialUsers = [
+        { name: 'Mark', username: 'Otto', level: '@mdo' },
+        { name: 'Jacob', username: 'Thornton', level: '@fat' },
+        { name: 'Larry the Bird', username: '', level: '@twitter' }
+    ];
+
+    return (
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            {drawerOpen && (
+                <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
+                    <Navigation />
+                </Layout.SideDrawer>
+            )}
+            <Stack
+                id="tab-bar"
+                direction="row"
+                justifyContent="space-around"
+                spacing={1}
+                sx={{
+                    display: { xs: 'flex', sm: 'none' },
+                    zIndex: '999',
+                    bottom: 0,
+                    position: 'fixed',
+                    width: '100dvw',
+                    py: 2,
+                    backgroundColor: 'background.body',
+                    borderTop: '1px solid',
+                    borderColor: 'divider',
+                }}
+            >
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/email/"
+                    size="sm"
+                    startDecorator={<EmailRoundedIcon />}
+                    sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+                >
+                    Email
+                </Button>
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    aria-pressed="true"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/team/"
+                    size="sm"
+                    startDecorator={<PeopleAltRoundedIcon />}
+                    sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+                >
+                    Team
+                </Button>
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/files/"
+                    size="sm"
+                    startDecorator={<FolderRoundedIcon />}
+                    sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+                >
+                    Files
+                </Button>
+            </Stack>
+            <Layout.Root
+                sx={{
+                    ...(drawerOpen && {
+                        height: '100vh',
+                        overflow: 'hidden',
+                    }),
+                }}
+            >
+                <Layout.Header>
+                    <Header />
+                </Layout.Header>
+                <Layout.SideNav>
+                    <Navigation />
+                </Layout.SideNav>
+                <Layout.Main>
+                    <UserList initialUsers={initialUsers} />
+                </Layout.Main>
+            </Layout.Root>
+        </CssVarsProvider>
+    );
+}
