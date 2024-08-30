eeb2d6d3f90347ff3ca04b053cea2650d9366bed
root <root@MatebookXpro>
2024-08-30 19:26:00 +1000

    HomePageCompleted

---
+++ frontend/app/homepage/page.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Button from '@mui/joy/Button';
+import Stack from '@mui/joy/Stack';
+
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import FolderRoundedIcon from '@mui/icons-material/FolderRounded';
+
+import Layout from '@/app/components/layout';
+import Header from '@/app/components/header';
+import Navigation from '@/app/components/navigation';
+
+import './homepage.css';
+
+
+export default function HomePage() {
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+    return (
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            {drawerOpen && (
+                <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
+                    <Navigation />
+                </Layout.SideDrawer>
+            )}
+            <Stack
+                id="tab-bar"
+                direction="row"
+                justifyContent="space-around"
+                spacing={1}
+                sx={{
+                    display: { xs: 'flex', sm: 'none' },
+                    zIndex: '999',
+                    bottom: 0,
+                    position: 'fixed',
+                    width: '100dvw',
+                    py: 2,
+                    backgroundColor: 'background.body',
+                    borderTop: '1px solid',
+                    borderColor: 'divider',
+                }}
+            >
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/email/"
+                    size="sm"
+                    startDecorator={<EmailRoundedIcon />}
+                    sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+                >
+                    Email
+                </Button>
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    aria-pressed="true"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/team/"
+                    size="sm"
+                    startDecorator={<PeopleAltRoundedIcon />}
+                    sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+                >
+                    Team
+                </Button>
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    component="a"
+                    href="/joy-ui/getting-started/templates/files/"
+                    size="sm"
+                    startDecorator={<FolderRoundedIcon />}
+                    sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+                >
+                    Files
+                </Button>
+            </Stack>
+            <Layout.Root
+                sx={{
+                    ...(drawerOpen && {
+                        height: '100vh',
+                        overflow: 'hidden',
+                    }),
+                }}
+            >
+                <Layout.Header>
+                    <Header />
+                </Layout.Header>
+                <Layout.SideNav>
+                    <Navigation />
+                </Layout.SideNav>
+                <Layout.Main>
+                    <div className="imageContainer">
+                        <img src="/OIClogo.png" alt="OIC Education Logo" className="image-logo" />
+                    </div>
+                    <div className="button-container">
+                        <Button className="button-item" variant="outlined" component="a" href="/userlist/">User List</Button>
+                        <Button className="button-item" variant="outlined" component="a" href="/surveymanagement/">Survey Management</Button>
+                        <Button className="button-item" variant="outlined" component="a" href="/dailymoods/">Daily Moods</Button>
+                        <Button className="button-item" variant="outlined" component="a" href="/news_notifications/">News and Notification</Button>
+                    </div>
+                </Layout.Main>
+            </Layout.Root>
+        </CssVarsProvider>
+    );
+}
